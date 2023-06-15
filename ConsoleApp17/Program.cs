68d1a45328969759b7620a2a224da87ea935e55e
aysa-sy <aisa.soleimani83@gmail.com>
2023-06-15 22:27:30 +0330

    Add project files.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void f(int n)
+        {
+            if (n == 1)
+                return 2;
+            else
+                return 2 * f(n - 1) + 1;
+        }
+
+        static void main(string[] args)
+        {
+            for (int i = 1; i <= 15; i++)
+                Console.WriteLine("f({0})={1}", i, f(i));
+        }
+    }
+}
